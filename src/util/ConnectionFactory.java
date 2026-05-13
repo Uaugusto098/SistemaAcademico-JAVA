9073782ee56d8ddce404b768ccf5656f42a0be57
Pedro augusto <pedroaugusto.borges36@gmail.com>
2026-05-12 22:17:48 -0300

    Estabelecendo conexão com o banco de dados

--- src/util/ConnectionFactory.java
+++ src/util/ConnectionFactory.java
@@ -1,5 +1,48 @@
 package util;
-
+import java.sql.*;
+import java.security.DomainCombiner;
 public class ConnectionFactory {
+	
+	public static Connection getConnection() throws Exception{
+		
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		String url="jdbc:mysql://localhost:3306/aluno_academico";
+		String login="root";
+		String senha="";
+		
+		return DriverManager.getConnection(url,login,senha);
+		
+	} 
+		catch(Exception e) {
+		throw new Exception(e.getMessage());
+	}
+	}
+	
+	
+	
+	public static void closeConnection(Connection conn) throws Exception{
+		close(conn);
+		
+	}
+	
+	private static void close(Connection conn) 
+		throws Exception{
+			
+			try {
+				conn.close();
+				
+			}
+			
+			catch(Exception e)
+			{
+				throw new Exception(e.getMessage());
+			}
+		}
+	}
 
-}
+
+
+
+
+	
