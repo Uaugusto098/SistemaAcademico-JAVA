ba4ad11f76c85b64b5c8e542e504887323045828
Pedro augusto <pedroaugusto.borges36@gmail.com>
2026-05-12 22:18:38 -0300

    Implementando teste de conexão ao banco de dados na Main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.sql.*;
 
+import util.ConnectionFactory;
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		try{
+			System.out.println("Tentando conexão no banco de dados");
+			Connection conn= ConnectionFactory.getConnection();
+			
+			if(conn!=null)
+			{
+				System.out.println("Conexão bem estabelecida com o banco de dados");
+				ConnectionFactory.closeConnection(conn);
+				System.out.println("Conexão fechada e testada!");
+			}
+		}catch(Exception e)
+		{
+			System.out.println("Falha na conexão: "+e.getMessage());
+		}
+			
+				
+				
+			
+			
+			
+			
+		}
 
 	}
 
-}
+
