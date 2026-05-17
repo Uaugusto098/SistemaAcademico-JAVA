1dcc06b3a4f1e1afb2fc9a00067a9e4f6cb08edd
Pedro augusto <pedroaugusto.borges36@gmail.com>
2026-05-17 16:02:41 -0300

    Atualizando a classe AlunoDAO

--- src/dao/AlunoDAO.java
+++ src/dao/AlunoDAO.java
@@ -1,11 +1,54 @@
 package dao;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import util.ConnectionFactory;
+import model.Aluno;
+
+
+
+
 
 public class AlunoDAO {
 	
 	
-	private ConnectionFactory conn;
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	private Aluno aluno; 
+	
+	
+	
+	
+	
+	public AlunoDAO() throws Exception{
+		
+		
+		try {
+			this.conn = ConnectionFactory.getConnection();
+			
+		}catch(Exception e) {
+			
+			throw new Exception("erro:\n"+e.getMessage());
+		}
+	}
+	
+	
+	public void atualizar(Aluno aluno) throws Exception{
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 
 }
